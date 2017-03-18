8918a37c0c95469132f042172fa37e46c8564c40
xrr2016 <13286904669@163.com>
2017-03-18 22:17:33 +0800

    :sparkles: :white_check_mark: Declaring a Winner

--- index.js
+++ index.js
@@ -8,11 +8,31 @@ const ReactDOM = window.ReactDOM
 //     )
 //   }
 // }
-//  生成随机id值
-function randomID () {
-  let str = ''
-  for (; str.length < 10; str += Math.random().toString(36).substr(2)) {}
-  return str.substr(0, 10)
+// 生成随机id值
+// function randomID () {
+//   let str = ''
+//   for (; str.length < 10; str += Math.random().toString(36).substr(2)) {}
+//   return str.substr(0, 10)
+// }
+// 计算赢者
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
 // 方格组件
 function Square (props) {
@@ -27,12 +47,15 @@ class Board extends React.Component {
   constructor () {
     super()
     this.state = {
-      squares: ['O', null, 'X', 'X', 'X', 'O', 'O', null, null],
+      squares: Array(9).fill(''),
       xIsNext: true
     }
   }
   handleClick (i) {
     const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
@@ -40,13 +63,19 @@ class Board extends React.Component {
     })
   }
   render () {
-    const status = `下一步是: ${this.state.xIsNext ? 'X' : 'O'}`
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = `Winner is ${winner}`
+    } else {
+      status = `下一步是: ${this.state.xIsNext ? 'X' : 'O'}`
+    }
     return (
       <div>
         <Status status={status} />
         <div className='game-board'>
-          {this.state.squares.map(val => {
-            return <Square key={randomID()} value={val} onClick={this.handleClick.bind(this)} />
+          {this.state.squares.map((val, index) => {
+            return <Square key={index} value={val} onClick={this.handleClick.bind(this, index)} />
           })}
         </div>
       </div>
